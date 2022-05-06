cbd9b3479cb8c27f302c26086925802c43294502
tiffpereira <tiffanympereira@gmail.com>
2022-05-06 15:15:14 -0400

    serializers created, and paths working for users and books

---
+++ tales_project/tales/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers 
+from .models import User, Book, Review 
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    reviews = serializers.HyperlinkedRelatedField(
+        view_name = 'review_detail',
+        many = True,
+        read_only = True 
+    )
+    class Meta: 
+        model = User 
+        fields = ('id', 'name', 'email', 'password', 'reviews')
+
+class BookSerializer(serializers.HyperlinkedModelSerializer):
+    reviews = serializers.HyperlinkedRelatedField(
+        view_name = 'review_detail',
+        many = True,
+        read_only = True 
+    )
+    class Meta: 
+        model = Book 
+        fields = ('id', 'title', 'author', 'summary', 'price', 'reviews')
+
+class ReviewSerializer(serializers.HyperlinkedModelSerializer):
+    users = serializers.HyperlinkedRelatedField(
+        view_name = 'user_detail',
+        read_only = True 
+    )
+    books = serializers.HyperlinkedRelatedField(
+        view_name = 'book_detail',
+        read_only = True 
+    )
+    class Meta: 
+        model = Review 
+        fields = ('id', 'name', 'title', 'body', 'books', 'users')
--- tales_project/tales/views.py
+++ tales_project/tales/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics 
+from .serializers import UserSerializer, BookSerializer, ReviewSerializer
+from .models import User, Book, Review 
 
 # Create your views here.
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class ReviewList(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
