4dc7e5aa781b861c7eaa048a5ff4ac544020c1e1
tiffpereira <tiffanympereira@gmail.com>
2022-05-06 13:20:24 -0400

    Set up models and admin for backend

---
+++ tales_project/tales/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name 
+
+class Book(models.Model):
+    title = models.CharField(max_length=50, default='no title')
+    author = models.CharField(max_length=50, default='no author')
+    genre = models.CharField(max_length=50, default='no genre')
+    summary = models.CharField(max_length=250, default='no summary')
+    price = models.CharField(max_length=50, default='no price')
+
+    def __str__(self):
+        return self.title 
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    body = models.CharField(max_length=250)
+    rating = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name 
