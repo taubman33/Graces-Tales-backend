0aa53a490f8a346e095e10784ade1461ca9f22f1
jataubman <jeremy.taubman@generalassemb.ly>
2022-12-05 14:57:26 -0500

    serializers

--- tales_project/tales/serializers.py
+++ tales_project/tales/serializers.py
@@ -1,35 +1,33 @@
-from rest_framework import serializers 
-from .models import User, Book, Review 
-
+from rest_framework import serializers
+from .models import User, Book, Review
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     reviews = serializers.HyperlinkedRelatedField(
         view_name = 'review_detail',
         many = True,
-        read_only = True 
+        read_only = True
     )
-    class Meta: 
-        model = User 
+    class Meta:
+        model = User
         fields = ('id', 'name', 'email', 'password', 'reviews')
 
-class BookSerializer(serializers.HyperlinkedModelSerializer):
-    reviews = serializers.HyperlinkedRelatedField(
-        view_name = 'review_detail',
-        many = True,
-        read_only = True 
-    )
-    class Meta: 
-        model = Book 
-        fields = ('id', 'title', 'author', 'summary', 'price', 'reviews', 'photo_url')
-
+        
 class ReviewSerializer(serializers.HyperlinkedModelSerializer):
     users = serializers.HyperlinkedRelatedField(
         view_name = 'user_detail',
-        read_only = True 
+        read_only = True
     )
     books = serializers.HyperlinkedRelatedField(
         view_name = 'book_detail',
-        read_only = True 
+        read_only = True
     )
-    class Meta: 
-        model = Review 
+    class Meta:
+        model = Review
         fields = ('id', 'name', 'title', 'body', 'books', 'users')
+class BookSerializer(serializers.HyperlinkedModelSerializer):
+    reviews = ReviewSerializer(
+        many = True,
+        read_only = True
+    )
+    class Meta:
+        model = Book
+        fields = ('id', 'title', 'author', 'summary', 'price', 'reviews', 'photo_url')
